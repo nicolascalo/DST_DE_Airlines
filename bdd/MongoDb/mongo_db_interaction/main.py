477a7ea2cd98e723005192586516ceab18b4bc5c
Cartomancloos <cloos-johan@hotmail.com>
2025-10-20 02:08:57 +0200

    code pour supprimer le document page des fichiers json

--- bdd/MongoDb/mongo_db_interaction/main.py
+++ bdd/MongoDb/mongo_db_interaction/main.py
@@ -1,6 +1,6 @@
 from .use_cases.insert_operational_flights_uc import insert
 from .repositories.operational_flights import create_index
-from .services.exploitation_json import open_json
+from .services.exploitation_json import open_json, delete_page_object_in_json
 from .services.folder_exploration import get_name_files_by_folder, get_folder_path_in_env
 import os
 from pathlib import Path
@@ -12,6 +12,7 @@ def import_operationalflights_in_mongodb():
     name_files = get_name_files_by_folder(folder_path)
     for name_file in name_files:
         json_file = open_json(name_file)
+        json_file = delete_page_object_in_json(json_file)
         insert(json_file)
     #create_index()
       
