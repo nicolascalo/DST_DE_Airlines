fb96e45ead80d57e60cf0d75f25ba92501f974a0
Cartomancloos <cloos-johan@hotmail.com>
2025-10-31 16:52:19 +0100

    fix get_all_csv to get the flights in csv format

--- bdd/MongoDb/start_api.sh
+++ bdd/MongoDb/start_api.sh
@@ -1,6 +1,3 @@
-set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$DIR/venv/bin/activate"
 
 
-uvicorn mongo_db_interaction.api:app --reload
+./venv/bin/uvicorn mongo_db_interaction.api:app --reload
