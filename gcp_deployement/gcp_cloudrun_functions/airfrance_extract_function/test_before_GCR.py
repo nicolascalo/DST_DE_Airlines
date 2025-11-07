2b4dc8642f13d279f367b7aaab8ad35b84d7a6e1
Yns-data <118600486+Yns-data@users.noreply.github.com>
2025-11-07 13:32:12 +0100

    Update CSV upload logic in test_before_GCR.py

--- gcp_deployement/gcp_cloudrun_functions/airfrance_extract_function/test_before_GCR.py
+++ gcp_deployement/gcp_cloudrun_functions/airfrance_extract_function/test_before_GCR.py
@@ -328,6 +328,8 @@ for call_parameter_csv in call_parameter_csv_list :
                 df_call_parameters_new = df_call_parameters_new.drop_duplicates(subset=parameter_list, keep='last')
                 csv_buffer = BytesIO()
                 df_call_parameters_new.fillna('').to_csv(csv_buffer,encoding="utf-8", index=0)
+                call_parameter_csv_blob.upload_from_string(csv_buffer.getvalue(), content_type="text/csv")
+                logger.info("df_call_parameters.csv updated")
 
                 
                 pageNumber = pageNumber + 1
@@ -356,6 +358,8 @@ for call_parameter_csv in call_parameter_csv_list :
                 
                 csv_buffer = BytesIO()
                 df_call_parameters_new.fillna('').to_csv(csv_buffer,encoding="utf-8", index=0)
+                call_parameter_csv_blob.upload_from_string(csv_buffer.getvalue(), content_type="text/csv")
+                logger.info("df_call_parameters.csv updated")
 
                 break
             
@@ -380,3 +384,5 @@ for call_parameter_csv in call_parameter_csv_list :
         df_call_parameters_new = pd.concat([df_call_parameters, df_call_parameters_date_update],ignore_index=True).fillna('').sort_values(['startRange','endRange','response'])
         csv_buffer = BytesIO()
         df_call_parameters_new.fillna('').to_csv(csv_buffer,encoding="utf-8", index=0)
+        call_parameter_csv_blob.upload_from_string(csv_buffer.getvalue(), content_type="text/csv")
+        logger.info("df_call_parameters.csv updated")
