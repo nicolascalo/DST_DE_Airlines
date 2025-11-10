7f3115d3b17eb708acf5dcd39f0d4ddf9ee43eab
Youssef-data <youssef.znati.it@gmail.com>
2025-11-10 09:41:23 +0100

    Update synthese.py

--- bdd/MongoDb/test_environment/synthese.py
+++ bdd/MongoDb/test_environment/synthese.py
@@ -8,21 +8,21 @@ df = pd.read_csv("csv_exploration.csv", low_memory=False)
 # Remplacer les crochets vides par des valeurs manquantes réelles
 df = df.replace('[]', np.nan)
 
-# === Suppression des colonnes inutiles ===
+# Suppression des colonnes inutiles
 cols_to_drop = ['_id', 'id', 'airline_code', 'airline_name']
 df = df.drop(columns=[col for col in cols_to_drop if col in df.columns])
 
-# === Colonnes liées aux irrégularités ===
+# Colonnes liées aux irrégularités
 # (ajuste ici les noms selon ton CSV si nécessaire)
 irregularity_cols = [
     'flightLegs_irregularity_delayDuration'
 ]
 irregularity_cols = [col for col in irregularity_cols if col in df.columns] # sécurité
 
-# === Création de la colonne indiquant la présence d'une irrégularité ===
+# Création de la colonne indiquant la présence d'une irrégularité 
 df['has_irregularity'] = df[irregularity_cols].notna().any(axis=1)
 
-# === Liste des colonnes à analyser ===
+# Liste des colonnes à analyser 
 category_cols = [
     'flightStatusPublic',
     'flightLegs_status',
@@ -32,7 +32,7 @@ category_cols = [
 ]
 category_cols = [col for col in category_cols if col in df.columns] # vérification des colonnes existantes
 
-# === Fonction d’analyse générique ===
+# Fonction d’analyse
 def analyse_irregularities_by(col_name):
     summary = (
         df.groupby(col_name)['has_irregularity']
