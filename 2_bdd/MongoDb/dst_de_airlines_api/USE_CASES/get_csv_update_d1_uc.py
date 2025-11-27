8c05e3a6f17a257d19f6b6b06cf306c03081beb2
Cartomancloos <cloos-johan@hotmail.com>
2025-11-27 17:01:16 +0100

    clean up

--- 2_bdd/MongoDb/dst_de_airlines_api/USE_CASES/get_csv_update_d1_uc.py
+++ 2_bdd/MongoDb/dst_de_airlines_api/USE_CASES/get_csv_update_d1_uc.py
@@ -7,7 +7,6 @@ from SERVICES.formater_service import format_json_flight_to_csv
 
 def get_csv_update_d1_by_id(nb_flights, id, date):
     date_time = datetime.now().strftime("%Y%m%d-%H-%M-%S")
-
     filename = f"afklm_update_d1_flight_from_mongo_filtered_{date_time}_{nb_flights}.csv.gz"
     flights = get_update_scheduled_d1(nb_flights, id, date)
     df = format_json_flight_to_csv(flights)
