72d78840e549e6991acd966b19201c127896c2c4
Nicolas CALO <nicolas.calo@yahoo.fr>
2025-11-28 18:36:09 +0100

    SAL -> Dashboard -> ML ok

--- 2_bdd/Postgre/docker-entrypoint-initdb.d/4_insert_mongodbdump.sql
+++ 2_bdd/Postgre/docker-entrypoint-initdb.d/4_insert_mongodbdump.sql
@@ -1,4 +1,4 @@
 
-COPY mongodb FROM '/tmp/afklm_historic_flight_from_mongo_filtered.csv' CSV HEADER;
-COPY mongodb FROM '/tmp/afklm_sched_flight_from_mongo_filtered.csv' CSV HEADER;
+COPY mongodb FROM '/tmp/data_input/afklm_historic_flight_from_mongo_filtered.csv' CSV HEADER;
+COPY mongodb FROM '/tmp/data_input/afklm_sched_flight_from_mongo_filtered.csv' CSV HEADER;
 
--- 3_ML/afklm_ml_api.py
+++ 3_ML/afklm_ml_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os, re, datetime, logging
 import pandas as pd
 from typing import Optional
@@ -21,8 +21,9 @@ import json
 import re
 from pydantic import BaseModel, create_model
 from typing import Any
-from fastapi.responses import JSONResponse, PlainTextResponse
-
+from fastapi.responses import JSONResponse, PlainTextResponse, FileResponse
+from sqlalchemy import create_engine
+import traceback
 import shutil
 import glob
 import subprocess
@@ -119,62 +120,67 @@ class Payload_flight(BaseModel):
     }
 
     """"Parameters of the flight for which to predict delay"""
-    _id: Optional[str] = None
-    id: Optional[str] = None
-    airline_code: Optional[str] = None
-    airline_name: Optional[str] = None
-    flightlegs_aircraft_ownerairlinecode: Optional[str] = None
-    flightlegs_aircraft_typecode: Optional[str] = None
-    arrival_continent: Optional[str] = None
-    arrival_subcontinent: Optional[str] = None
-    arrival_country: Optional[str] = None
-    arrival_location: Optional[str] = None
-    arrival_airport: Optional[str] = None
-    flightlegs_arrinfo_airport_code: str
-    flightlegs_arrinfo_airport_location_latitude: Optional[float] = None
-    flightlegs_arrinfo_airport_location_longitude: Optional[float] = None
-    flightlegs_arrinfo_times_scheduled: str
-    departure_continent: Optional[str] = None
-    departure_subcontinent: Optional[str] = None
-    departure_country: Optional[str] = None
-    departure_location: Optional[str] = None
-    departure_airport: Optional[str] = None
-    flightlegs_depinfo_airport_code: str
-    flightlegs_depinfo_airport_location_latitude: Optional[float] = None
-    flightlegs_depinfo_airport_location_longitude: Optional[float] = None
-    flightlegs_depinfo_airport_places_depposterm_gatenumber: Optional[str] = None
-    flightlegs_depinfo_times_scheduled: str
-    flightlegs_scheduledflightduration: Optional[str] = None
-    flightlegs_servicetype: Optional[str] = None
-    flightlegs_servicetypename: Optional[str] = None
-    flightlegs_status: Optional[str] = None
-    flightlegs_statusname: Optional[str] = None
-    flightnumber: Optional[int] = None
-    flightstatuspublic: Optional[str] = None
-    flightlegs_arrinfo_times_estimated_value: Optional[str] = None
-    flightlegs_arrinfo_times_latestpublished: Optional[str] = None
-    flightlegs_depinfo_times_actual: Optional[str] = None
-    flightlegs_depinfo_times_latestpublished: Optional[str] = None
-    flightlegs_arrinfo_airport_places_arrivalpositionterminal: Optional[str] = None
-    flightlegs_depinfo_airport_places_depposterm_boardingterminal: Optional[str] = None
+    flight_id	: Optional[str] = None
+    flightnumber	: Optional[int] = None 
+    airline_code	: Optional[str] = None
+    airline_name	: Optional[str] = None
+    flightlegs_aircraft_typecode	: Optional[str] = None
+    flightlegs_servicetypename	: Optional[str] = None
+    flightlegs_depinfo_airport_continent_name	: Optional[str] = None
+    flightlegs_depinfo_airport_subcontinent_name	: Optional[str] = None
+    flightlegs_depinfo_airport_country_code	: Optional[str] = None
+    flightlegs_depinfo_airport_country_name	: Optional[str] = None
+    flightlegs_depinfo_airport_airport_name	: Optional[str] = None
+    flightlegs_depinfo_airport_code	: str
+    flightlegs_depinfo_airport_places_depposterm_boardingterminal	: Optional[str] = None
+    flightlegs_depinfo_airport_places_depposterm_gatenumber	: Optional[str] = None
+    flightlegs_depinfo_times_scheduled_date	: str
+    flightlegs_depinfo_times_scheduled_time	: str
+    flightlegs_depinfo_times_scheduled_year	: Optional[int] = None 
+    flightlegs_depinfo_times_scheduled_month	: Optional[int] = None 
+    flightlegs_depinfo_times_scheduled_day	: Optional[int] = None 
+    flightlegs_depinfo_times_scheduled_hour	: Optional[int] = None 
+    flightlegs_depinfo_times_scheduled_minute	: Optional[int] = None 
+    flightlegs_depinfo_times_scheduled_timezone	: str
+    flightlegs_depinfo_times_number_week	: Optional[int] = None 
+    flightlegs_arrinfo_airport_continent_name	: Optional[str] = None
+    flightlegs_arrinfo_airport_subcontinent_name	: Optional[str] = None
+    flightlegs_arrinfo_airport_country_code	: Optional[str] = None
+    flightlegs_arrinfo_airport_country_name	: Optional[str] = None
+    flightlegs_arrinfo_airport_airport_name	: Optional[str] = None
+    flightlegs_arrinfo_airport_code	: str
+    flightlegs_arrinfo_airport_places_arrivalpositionterminal	: Optional[str] = None
+    flightlegs_arrinfo_times_scheduled_date	: str
+    flightlegs_arrinfo_times_scheduled_time	: str
+    flightlegs_arrinfo_times_scheduled_year	: Optional[int] = None 
+    flightlegs_arrinfo_times_scheduled_month	: Optional[int] = None 
+    flightlegs_arrinfo_times_scheduled_day	: Optional[int] = None 
+    flightlegs_arrinfo_times_scheduled_hour	: Optional[int] = None 
+    flightlegs_arrinfo_times_scheduled_minute	: Optional[int] = None 
+    flightlegs_arrinfo_times_scheduled_timezone	: str
+    flightlegs_arrinfo_times_number_week	: Optional[int] = None 
 
 
 
 
+with open('./config/api_test_payload.json') as json_file:
+    json_example_payload = json.load(json_file)
+'''
 try:
     with open('./config/api_test_payload.json') as json_file:
-        json_example_payload = json.load(json_file)
+        json_example = json.load(json_file)
 
     
 except:
-    json_example_payload =  {"_id":"690c974f228ea0580c98a8be","id":"20250517+G3+7612","airline_code":"G3","airline_name":"GOL LINHAS AEREAS S.A.","flightLegs_aircraft_ownerAirlineCode":"G3","flightLegs_aircraft_typeCode":"7M8","flightLegs_arrivalInformation_airport_city_country_areaCode":"I","flightLegs_arrivalInformation_airport_city_country_code":"AR","flightLegs_arrivalInformation_airport_city_country_name":"ARGENTINA","flightLegs_arrivalInformation_airport_code":"COR","flightLegs_arrivalInformation_airport_location_latitude":-31.3131,"flightLegs_arrivalInformation_airport_location_longitude":-64.1994,"flightLegs_arrivalInformation_times_scheduled":"2025-05-17T02:20:00.000-03:00","flightLegs_departureInformation_airport_city_country_areaCode":"I","flightLegs_departureInformation_airport_city_country_code":"BR","flightLegs_departureInformation_airport_city_country_name":"BRAZIL","flightLegs_departureInformation_airport_code":"GIG","flightLegs_departureInformation_airport_location_latitude":-22.8214,"flightLegs_departureInformation_airport_location_longitude":-43.2494,"flightLegs_departureInformation_airport_places_departurePositionTerminal_gateNumber":"","flightLegs_departureInformation_times_scheduled":"2025-05-16T22:30:00.000-03:00",
+    json_example =  {"_id":"690c974f228ea0580c98a8be","id":"20250517+G3+7612","airline_code":"G3","airline_name":"GOL LINHAS AEREAS S.A.","flightLegs_aircraft_ownerAirlineCode":"G3","flightLegs_aircraft_typeCode":"7M8","flightLegs_arrivalInformation_airport_city_country_areaCode":"I","flightLegs_arrivalInformation_airport_city_country_code":"AR","flightLegs_arrivalInformation_airport_city_country_name":"ARGENTINA","flightLegs_arrivalInformation_airport_code":"COR","flightLegs_arrivalInformation_airport_location_latitude":-31.3131,"flightLegs_arrivalInformation_airport_location_longitude":-64.1994,"flightLegs_arrivalInformation_times_scheduled":"2025-05-17T02:20:00.000-03:00","flightLegs_departureInformation_airport_city_country_areaCode":"I","flightLegs_departureInformation_airport_city_country_code":"BR","flightLegs_departureInformation_airport_city_country_name":"BRAZIL","flightLegs_departureInformation_airport_code":"GIG","flightLegs_departureInformation_airport_location_latitude":-22.8214,"flightLegs_departureInformation_airport_location_longitude":-43.2494,"flightLegs_departureInformation_airport_places_departurePositionTerminal_gateNumber":"","flightLegs_departureInformation_times_scheduled":"2025-05-16T22:30:00.000-03:00",
                 "flightLegs_scheduledFlightDuration":"PT3H50M",
                  "flightLegs_serviceType":"J","flightLegs_serviceTypeName":"Normal Service","flightNumber":7612,"flightLegs_departureInformation_airport_places_departurePositionTerminal_boardingTerminal":"",
                  "flightLegs_arrivalInformation_airport_places_arrivalPositionTerminal":""}
 
+'''
 
 
-Payload_flight.model_config["json_schema_extra"]["examples"] = [json_example_payload]
+Payload_flight.model_config["json_schema_extra"]["examples"] = json_example_payload
 
 
 
@@ -246,6 +252,62 @@ def get_model_parameters():
     return JSONResponse(content=best_models_metrics_json) 
 
 
+@api.get('/retrain_models', name="Retrain the machine learning models with the current dataset", tags=['training'],response_class=PlainTextResponse)
+def retrain_models():
+    """Retrain the machine learning models with the current dataset"""
+    subprocess.run(["python", "afklm_ml_training.py"])
+    return "Model training over"
+
+@api.get('/retrieve_latest_data_for_training', name="Retrieve the latest training dataset", tags=['training'],response_class=PlainTextResponse)
+def retrieve_latest_training_dataset():
+
+    username = 'postgres'
+    password = 'postgres'
+    host = 'dst_de_airlines_postgres'
+    port = '5432'
+    database_name = 'afklm'
+
+    DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database_name}"
+
+
+
+
+    query = 'select v_past_flight.flight_id,  v_past_flight.flightNumber,  v_past_flight.airline_code,  v_past_flight.airline_name,  v_past_flight.flightStatusPublic,  v_past_flight.flightLegs_aircraft_typeCode,  v_past_flight.flightLegs_scheduledFlightDuration,  v_past_flight.flightLegs_serviceType,  v_past_flight.flightLegs_aircraft_ownerAirlineCode,  v_past_flight.flightLegs_status,  v_past_flight.delay_status,  v_past_flight.flightLegs_serviceTypeName, v_past_flight.flightLegs_publishedStatus, v_past_flight.flightLegs_legStatusPublic, v_past_flight.flightLegs_statusName, v_past_flight.flightLegs_irregularity_delayDuration, v_past_flight.flightlegs_irregularity_delayduration_total, v_past_flight.flightLegs_irregularity_delayInfo_delayReasonPublicLong, v_past_flight.flightLegs_irregularity_delayInfo_delayReasonPublicShort, v_geod.flightLegs_depInfo_airport_Continent_Name,  v_geod.flightLegs_depInfo_airport_Subcontinent_Name,  v_geod.flightLegs_depInfo_airport_Country_Code,  v_geod.flightLegs_depInfo_airport_Country_Name,  v_geod.flightLegs_depInfo_airport_Location_name,  v_geod.flightLegs_depInfo_airport_Airport_Name,  v_geod.flightLegs_depInfo_airport_Icao_Code,  v_geod.flightLegs_depInfo_airport_Latitude,  v_geod.flightLegs_depInfo_airport_Longitude, v_past_flight.flightLegs_depInfo_airport_code, v_past_flight.flightLegs_depInfo_airport_places_depPosTerm_boardingTerminal, v_past_flight.flightLegs_depInfo_airport_places_depPosTerm_gateNumber, v_past_flight.flightLegs_depInfo_times_scheduled_date, v_past_flight.flightLegs_depInfo_times_scheduled_time, v_past_flight.flightLegs_depInfo_times_scheduled_year, v_past_flight.flightLegs_depInfo_times_scheduled_month, v_past_flight.flightLegs_depInfo_times_scheduled_day , v_past_flight.flightLegs_depInfo_times_scheduled_hour, v_past_flight.flightLegs_depInfo_times_scheduled_minute, v_past_flight.flightLegs_depInfo_times_scheduled_timezone, v_past_flight.flightLegs_depInfo_times_number_week, v_geoa.flightLegs_arrInfo_airport_Continent_Name,  v_geoa.flightLegs_arrInfo_airport_Subcontinent_Name,  v_geoa.flightLegs_arrInfo_airport_Country_Code,  v_geoa.flightLegs_arrInfo_airport_Country_Name,  v_geoa.flightLegs_arrInfo_airport_Location_name,  v_geoa.flightLegs_arrInfo_airport_Airport_Name,  v_geoa.flightLegs_arrInfo_airport_Icao_Code,  v_geoa.flightLegs_arrInfo_airport_Latitude,  v_geoa.flightLegs_arrInfo_airport_Longitude, v_past_flight.flightLegs_arrInfo_airport_code, v_past_flight.flightLegs_arrInfo_airport_places_arrivalPositionTerminal, v_past_flight.flightLegs_arrInfo_times_scheduled_date, v_past_flight.flightLegs_arrInfo_times_scheduled_time, v_past_flight.flightLegs_arrInfo_times_scheduled_year, v_past_flight.flightLegs_arrInfo_times_scheduled_month, v_past_flight.flightLegs_arrInfo_times_scheduled_day, v_past_flight.flightLegs_arrInfo_times_scheduled_hour, v_past_flight.flightLegs_arrInfo_times_scheduled_minute, v_past_flight.flightLegs_arrInfo_times_scheduled_timezone, v_past_flight.flightLegs_arrInfo_times_number_week from v_past_flight v_past_flight 	INNER JOIN v_geod v_geod ON v_geod.flightLegs_depInfo_airport_Iata_Code = v_past_flight.flightLegs_depInfo_airport_code 	INNER JOIN v_geoa v_geoa ON v_geoa.flightLegs_arrInfo_airport_Iata_Code = v_past_flight.flightLegs_arrInfo_airport_code where v_past_flight.flightLegs_depInfo_times_scheduled_date < current_date;'
+
+
+
+    #df = pd.read_csv('afklm_flight_from_mongo_filtered_20251113-21-36-51_test.csv', low_memory=False)
+    try:
+        engine = create_engine(DATABASE_URL)
+
+        def get_sql_data(query):
+            df = pd.read_sql(query, engine)
+            return df
+
+        try:
+            df = get_sql_data(query)
+
+        except Exception as e:
+            full_trace = traceback.format_exc()
+            print(full_trace)  # shows full stack trace in Docker logs
+
+            raise HTTPException(
+                status_code=500,
+                detail=f"Database query failed: {str(e)}"
+            )
+
+        df.to_csv('./data/afklm_past_flight.csv.zip', compression='zip', index=False)
+        print("PostgreSQL data retrieved")
+
+    except Exception as e:
+        full_trace = traceback.format_exc()
+        print(full_trace)
+
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database connection error: {str(e)}"
+        )
+
 
 @api.get('/training_parameters_show', name="", tags=['training'])
 def get_training_parameters():
@@ -253,11 +315,44 @@ def get_training_parameters():
     return JSONResponse(ml_training_settings)
 
 
-@api.get('/display_training_log', name="", tags=['training'],response_class=PlainTextResponse)
+@api.post('/training_parameters_upload', name="", tags=['training'],response_class=PlainTextResponse)
+def set_training_parameters(parameters: PayloadTrainingParameters):
+    """Set current parameters for model training"""
+    new_params = parameters.model_dump()
+    
+    with open(f"./config/afklm_ml_training_settings.json", 'w', encoding='utf-8') as f:
+        params =json.dump(new_params, f, ensure_ascii=False, indent=4)
+
+    return("Training parameters updated")
+
+@api.get('/training_parameters_defaults', name="", tags=['training'],response_class=PlainTextResponse)
+def set_training_parameters_to_default():
+    """Reset model parameters to default"""
+    shutil.copyfile("./config/afklm_ml_training_settings_default.json","./config/afklm_ml_training_settings.json")
+    return "Training paramters reset to defaults"
+
+
+@api.get('/display_training_run_list', name="", tags=['training_outputs'],response_class=JSONResponse)
+def list_training_logs():
+    """Display the list of training runs"""
+    try :
+        log_list = glob.glob('./*/*/*ML.log', recursive=True)
+        log_list.sort(reverse=True)
+        log_list = [re.sub(".*/","",file) for file in log_list]
+        log_list = [re.sub("_ML\\..*","",file) for file in log_list]
+        log_list=dict(enumerate(log_list))
+        print(log_list)
+        return log_list
+    except:
+
+        HTTPException(status_code=404, detail="No log found")
+
+
+@api.get('/display_last_training_log', name="", tags=['training_outputs'],response_class=PlainTextResponse)
 def display_training_log():
     """Display the log of the last training"""
     try :
-        log_list = glob.glob('./*/*/*.log', recursive=True)
+        log_list = glob.glob('./*/*/*ML.log', recursive=True)
         log_list.sort(reverse=True)
         last_log_file = log_list[0]
         with open(last_log_file, 'r') as f:
@@ -265,7 +360,41 @@ def display_training_log():
         return last_log
     except:
 
-        print("No log found")
+        HTTPException(status_code=404, detail="No log found")
+
+@api.get('/download_chosen_training_run', name="", tags=['training_outputs'])
+def download_chosen_training_run(run:str):
+    """Downloads the chosen training run. Will not include any model as only the best models are kept."""
+    try :
+        file_name = f"{run}"
+        file_path = f"./outputs/{run}"
+        shutil.make_archive(f"./outputs/{file_name}", 'zip', file_path)
+
+
+        return FileResponse(path=f"./outputs/{file_name}.zip", filename=f"{file_name}"+'.zip', media_type='application/zip')
+    except:
+
+        HTTPException(status_code=404, detail="No run found for this name")
+
+
+
+
+@api.get('/download_best_models', name="", tags=['training_outputs','predictions'])
+def download_chosen_training_run():
+    """Downloads the best overall models."""
+    try :
+        file_name = "afklm_ml_best_models"
+        file_path = f"./outputs/best_models"
+        shutil.make_archive(f"./outputs/{file_name}", 'zip', file_path)
+
+
+        return FileResponse(path=f"./outputs/{file_name}.zip", filename=f"{file_name}"+'.zip', media_type='application/zip')
+    except:
+
+        HTTPException(status_code=404, detail="No run found for this name")
+
+
+
 
 
 @api.post('/get_delay_predictions', name = "Get flight delay predictions", tags=['predictions'])
@@ -282,10 +411,21 @@ def post_users(parameters: Payload_flight):
     # DATA CLEANING
     # --------------------------------------
 
-    entry["company_flight"] = entry["id"].apply(lambda x: re.sub("^.*?\\+","", x))
+    entry["company_flight"] = entry["flight_id"].apply(lambda x: re.sub("^.*?\\+","", x))
 
 
     entry_cleaned = entry
+    entry_cleaned['flightlegs_arrinfo_times_scheduled'] = entry_cleaned.apply(
+        lambda row:row.flightlegs_arrinfo_times_scheduled_date+"T"+row.flightlegs_depinfo_times_scheduled_time+".000"+row.flightlegs_arrinfo_times_scheduled_timezone
+                    ,
+        axis=1
+    )
+
+    entry_cleaned['flightlegs_depinfo_times_scheduled'] = entry_cleaned.apply(
+        lambda row: row.flightlegs_depinfo_times_scheduled_date+"T"+row.flightlegs_depinfo_times_scheduled_time+".000"+row.flightlegs_depinfo_times_scheduled_timezone
+                    ,
+        axis=1
+    )
 
 
     entry_cleaned["flightlegs_scheduledFlightDuration"] = entry_cleaned.apply(
@@ -302,6 +442,8 @@ def post_users(parameters: Payload_flight):
 
 
     season_dictionary = {1:'winter',2:'winter',3:'spring',4:'spring',5:'spring',6:'summer',7:'summer',8:'summer',9:'fall',10:'fall',11:'fall',12:'winter'}
+
+
 
 
     entry_cleaned['flightlegs_season'] = entry_cleaned.apply(
@@ -356,7 +498,7 @@ def post_users(parameters: Payload_flight):
             raise RuntimeError(f"STATUS MODEL ERROR: {e}")            
         try:
             prediction_delay_regresssion = best_model_regression.predict(entry_cleaned)
-            prediction_delay_regresssion = prediction_delay_regresssion[0]
+            prediction_delay_regresssion = prediction_delay_regresssion[0].item()
         except Exception as e:
             prediction_delay_regresssion = "No model found for delay duration regression"
             raise RuntimeError(f"STATUS MODEL ERROR: {e}")            
